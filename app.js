4dd945844194353ed8dd6ed44a6652b633763877
Shivam <samsersingh3133s@gmail.com>
2023-03-08 20:04:08 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const https = require('https');
+const ejs =require('ejs');
+const exp = require('constants');
+
+const app = express();
+app.set("view engine", 'ejs');
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.get("/", function (req, res) {
+    // res.send("ok");
+    // res.sendFile(__dirname+"/index.html") 
+    // res.render("index");
+    // res.render('index', { foo: 'FOO' });
+    res.render('index', { myjoke:joke, creator:created_by });
+
+    var joke;
+    var created_by;
+    const url = "https://hindi-jokes-api.onrender.com/jokes?api_key=dc8ca44ff3d3788d2dcd56823843";
+
+    https.get(url, function (responce) {
+
+        // console.log(responce);
+        console.log("status" + responce.statusCode);
+        console.log(url);
+        responce.on("data", function (data) {
+            // console.log(data);
+            let JokeData = JSON.parse(data);
+            // console.log(JokeData.jokeContent);
+             joke=JokeData.jokeContent;
+            //  console.log(JokeData);
+             console.log(JokeData.created_by);
+             created_by=JokeData.created_by;
+            console.log(joke);
+            // responce.rander(joke: Myjoke);
+            
+        });
+        console.log("Shiva ");
+    })
+
+    console.log("Shivam ");
+});
+
+app.listen(3000, function () {
+    console.log("App is running on port 3000");
+}); 
